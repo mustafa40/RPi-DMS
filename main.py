a7f80ff83505e611eb4dd3e06e75078a90e73963
Mustafa Erdem Memiş <mustafa_erdem40@hotmail.com>
2026-07-03 10:06:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dashboard import Dashboard
 
 
 camera = CameraManager(camera_index=0, width=640, height=480, fps=15, flip=True)
-face_detector = FaceDetector(memory_time=1.2)
+face_detector = YuNetFaceDetector()
 eye_detector = EyeDetector()
 head_detector = HeadPoseDetector(frame_height=480, down_threshold=0.05, hold_time=0.3)
 engine = FatigueEngine(alarm_time=1.2)
