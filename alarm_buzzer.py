80a25123ec52a60c1c46c0f9770545e39d6b0489
Mustafa Erdem Memiş <mustafa_erdem40@hotmail.com>
2026-07-03 11:43:14 +0300

    Update alarm_buzzer.py

--- alarm_buzzer.py
+++ alarm_buzzer.py
@@ -1,3 +1,4 @@
+import time
 from gpiozero import Buzzer
 
 
@@ -5,17 +6,44 @@ class AlarmBuzzer:
     def __init__(self, gpio_pin=18):
         self.buzzer = Buzzer(gpio_pin)
         self.active = False
+        self.last_toggle = time.time()
+        self.beep_state = False
 
     def on(self):
-        if not self.active:
-            self.buzzer.on()
-            self.active = True
+        self.buzzer.on()
+        self.active = True
 
     def off(self):
-        if self.active:
-            self.buzzer.off()
-            self.active = False
-
-    def cleanup(self):
         self.buzzer.off()
         self.active = False
+        self.beep_state = False
+
+    def update(self, alert_level):
+        now = time.time()
+
+        if alert_level == "ALARM":
+            # Gerçek DMS gibi: bip bip bip
+            if now - self.last_toggle >= 0.25:
+                self.last_toggle = now
+                self.beep_state = not self.beep_state
+
+                if self.beep_state:
+                    self.buzzer.on()
+                else:
+                    self.buzzer.off()
+
+        elif alert_level == "WARNING":
+            # Uyarı seviyesinde kısa aralıklı hafif bip
+            if now - self.last_toggle >= 0.8:
+                self.last_toggle = now
+                self.beep_state = not self.beep_state
+
+                if self.beep_state:
+                    self.buzzer.on()
+                else:
+                    self.buzzer.off()
+        else:
+            self.off()
+
+    def cleanup(self):
+        self.off()
