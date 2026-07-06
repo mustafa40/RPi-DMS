213cb1cf081a4edd81d6252773fbcf25ab2413ea
Mustafa Erdem Memiş <mustafa_erdem40@hotmail.com>
2026-07-06 16:18:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import cv2
-import os
-import numpy as np
 import config
 
 from camera_manager import CameraManager
@@ -13,6 +11,7 @@ from dashboard import Dashboard
 from blink_tracker import BlinkTracker
 from perclos_tracker import PerclosTracker
 from fatigue_score import FatigueScore
+from splash_screen import show_splash
 
 
 def get_screen_size():
@@ -26,28 +25,6 @@ def get_screen_size():
         return w, h
     except Exception:
         return 800, 480
-
-
-def show_splash(window_name, screen_w, screen_h):
-    splash_path = os.path.join(os.path.dirname(__file__), config.SPLASH_IMAGE)
-    img = cv2.imread(splash_path)
-
-    if img is None:
-        img = np.ones((screen_h, screen_w, 3), dtype=np.uint8) * 255
-        cv2.putText(img, "MARTUR FOMPAK INTERNATIONAL",
-                    (40, screen_h // 2 - 20),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1.0, (80, 80, 80), 2)
-
-        cv2.putText(img, "Connectivity & Smart Devices",
-                    (80, screen_h // 2 + 35),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.8, (100, 100, 100), 2)
-    else:
-        img = cv2.resize(img, (screen_w, screen_h))
-
-    cv2.imshow(window_name, img)
-    cv2.waitKey(config.SPLASH_TIME_MS)
 
 
 def main():
@@ -73,7 +50,6 @@ def main():
 
     face_detector = FaceDetector(memory_time=1.2)
     eye_detector = EyeDetector()
-
     driver_state_analyzer = DriverStateAnalyzer(
         frame_height=config.DRIVER_STATE_FRAME_HEIGHT
     )
@@ -150,7 +126,6 @@ def main():
             fatigue_score = fatigue_score_engine.update(driver_state, perclos)
 
             status, alert_level = engine.update(driver_state)
-
             alarm.update(alert_level)
 
             frame = dashboard.draw(
