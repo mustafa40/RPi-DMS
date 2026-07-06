2c9f80131889753394a5e7d436d1583cf887a4ae
Mustafa Erdem Memiş <mustafa_erdem40@hotmail.com>
2026-07-06 18:09:08 +0300

    Update test_max30102.py

--- test_max30102.py
+++ test_max30102.py
@@ -5,19 +5,18 @@ from max30102_reader import MAX30102Reader
 sensor = MAX30102Reader()
 sensor.setup()
 
-print("MAX30102 test basladi.")
+print("MAX30102 basladi")
 print("Part ID:", hex(sensor.part_id()))
-print("Parmagini sensorun uzerine hafif bastir.")
-print("IR 10000 ustune cikarsa parmak algilaniyor demektir.")
+print("Parmagini sensorun ustune koy")
 print()
 
 while True:
     red, ir = sensor.read_latest()
 
     if red is None:
-        print("FIFO bos...")
+        print("FIFO BOS")
     else:
-        finger = "FINGER" if ir > 10000 else "NO FINGER"
-        print(f"RED: {red:7d} | IR: {ir:7d} | {finger}")
+        finger = "FINGER" if ir > 5000 else "NO FINGER"
+        print(f"RED: {red:8d} | IR: {ir:8d} | {finger}")
 
     time.sleep(0.1)
